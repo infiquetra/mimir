fbb6192b177bf19871060b8e1cf4f7ef712ee5bd
Jeffrey Cox <namredips@gmail.com>
2025-12-08 09:53:08 -0500

    fix: add app_database import for WalletTransactionsCompanion
    The drift import alone wasn't enough - we need app_database.dart
    to access the generated Companion classes.

--- integration_test/bug_regression/eve_id_resolution_test.dart
+++ integration_test/bug_regression/eve_id_resolution_test.dart
@@ -2,6 +2,7 @@ import 'package:drift/drift.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:mimir/core/database/app_database.dart';
 import 'package:mimir/features/skills/presentation/skills_screen.dart';
 import 'package:mimir/features/wallet/presentation/wallet_screen.dart';
 
