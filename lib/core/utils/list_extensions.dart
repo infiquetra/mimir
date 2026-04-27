fc0bb8b75f8d24b87701066d7c7b65e3d9164271
Artemis <artemis@infiquetra.com>
2026-04-27 17:03:04 +0000

    feat(collections): add chunked extension method to List (#378)

---
+++ lib/core/utils/list_extensions.dart
@@ -0,0 +1,19 @@
+library list_extensions;
+
+extension ChunkedList<T> on List<T> {
+  /// Splits the list into consecutive sub-lists of at most [size] elements.
+  ///
+  /// Throws [ArgumentError] if [size] is less than 1.
+  List<List<T>> chunked(int size) {
+    if (size < 1) {
+      throw ArgumentError.value(size, 'size', 'Must be greater than or equal to 1');
+    }
+
+    final List<List<T>> chunks = <List<T>>[];
+    for (int start = 0; start < length; start += size) {
+      final int end = start + size < length ? start + size : length;
+      chunks.add(sublist(start, end));
+    }
+    return chunks;
+  }
+}
